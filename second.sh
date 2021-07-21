708053e1dae0e479a436708622a9ae6ad41db67c
Ubuntu <student@ML-RefVm-511909.5rza2ip45goexobibe050wxovg.qx.internal.cloudapp.net>
2021-07-21 06:17:06 +0000

    Update to second.sh

--- second.sh
+++ second.sh
@@ -1,4 +1,13 @@
+# Make sure bash shell is used
+
 #!/bin/bash
+
+# Display "Hi There" on screen. note - type username at end of line
+
 echo "Hi there!"
-echo "It's good to see you $1!"
-exit 25
+
+#Display "Its good to see you" followed by $1 - variable entered in previous line
+
+echo  "It's good to see you $1!"
+
+exit 0
